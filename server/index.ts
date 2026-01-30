11240f09731a110358dfde1672be5c8ef294c52d
JuniorTorresH <jtorresh.92@gmail.com>
2026-01-30 11:11:56 -0500

    rutas estáticas para Vercel

--- server/index.ts
+++ server/index.ts
@@ -87,7 +87,7 @@ async function initializeServer() {
   if (process.env.NODE_ENV === "production") {
     serveStatic(app);
   } else {
-    const { setupVite } = await import("./vite");
+    const { setupVite } = await import("./vite.js");
     await setupVite(httpServer, app);
   }
 
